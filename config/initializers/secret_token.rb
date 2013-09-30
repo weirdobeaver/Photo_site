ffe72898b2ae800ed8def46ce4e5e83816cdbef0
weirdobeaver <weirdobeaver88@gmail.com>
2013-09-30 11:35:08 +0200

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photosite::Application.config.secret_key_base = 'f9bf6e863ecdf0741cd3cfa9e92b391b8879df027083e14cde4ed1d64779e726c06841512f212bb69e08891f0f588e60eb1c4b65d360fc35e1525926f6b72a62'
