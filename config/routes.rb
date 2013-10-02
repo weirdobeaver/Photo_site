7bc22bc2725a0f9cec3954f8ec77038bc1140b33
weirdobeaver <weirdobeaver88@gmail.com>
2013-10-02 10:07:44 +0200

    Navbar color malfunction

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Photosite::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/portfolio"
+  get "static_pages/offer"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
