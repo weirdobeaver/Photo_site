d10a117daab4a51b8abddd02fb7485cb8c24a478
weirdobeaver <weirdobeaver88@gmail.com>
2013-11-05 16:19:46 +0100

    Show action and view working

--- app/controllers/photo_adds_controller.rb
+++ app/controllers/photo_adds_controller.rb
@@ -2,8 +2,8 @@ class PhotoAddsController < ApplicationController
   before_action :signed_in_user, except: [:index]
   before_action :correct_user,   only: :destroy
 
-	def index
-    @photo_adds = PhotoAdd.paginate(:page => params[:page], :per_page => 1)
+	def show
+    @photo_add = PhotoAdd.find_by(params[:id])
     @current_page = "/portfolio"
 	end
 
